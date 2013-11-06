04928003c727378987eb73dc45b25c7060c861ec
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2013-11-05 22:26:55 -0500

    added more end to end tests

--- test/e2e.js
+++ test/e2e.js
@@ -4,8 +4,17 @@ var path = require('path');
 var cc = path.join(__dirname, '../index.js');
 var examples = path.join(__dirname, '../examples');
 
-gt.async('basic', 1, function () {
+gt.async('local file', 1, function () {
     var url = path.join(examples, 'basic/index.html');
     gt.exec('node', [cc, '--input', url], 2, 'basic page with 2 errors');
 }, 4000);
 
+gt.async('local file as default argument', 1, function () {
+    var url = path.join(examples, 'basic/index.html');
+    gt.exec('node', [cc, url], 2, 'basic page with 2 errors');
+}, 4000);
+
+gt.async('google', 1, function () {
+    var url = 'http://google.com';
+    gt.exec('node', [cc, url], 0, 'google is fine');
+}, 4000);
