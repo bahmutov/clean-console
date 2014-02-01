e029307170bc4806705eca81f7a4fdaacf706bd8
Gleb Bahmutov <gleb@kensho.com>
2014-02-01 10:36:16 -0500

    running jshint using gulp, fixes #9

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('lint', function() {
+  gulp.src(['*.js', 'lib/*.js', 'test/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('default', ['lint']);
